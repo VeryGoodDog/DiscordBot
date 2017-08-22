23433777ba80cc24d70971f0238795dba100fa18
The Grand Dog <alex.h@me.com>
2017-08-22 09:22:37 -0400

    numbers subtract working fully

--- commands/numbers/subCom/subtract/index.js
+++ commands/numbers/subCom/subtract/index.js
@@ -1,5 +1,6 @@
 exports.func = (client, msg, args) => {
-  if (typeof args[0] !== 'number' || typeof args[1] !== 'number') return msg.channel.send(`${args[0]} or ${args[1]} is not a number.`);
-  let numOne = args[0], numTwo = args[1];
-  msg.channel.send(`${numOne} - ${numTwo} = ${numOne - numTwo}`)
+  let numOne = Number(args[0]) ? Number(args[0]) : 'wow';
+  let numTwo = Number(args[1]) ? Number(args[1]) : 'u need a number';
+  let diff = (typeof numOne === 'number' && typeof numOne === 'number') ? numOne - numTwo : 'gimmeth numbr';
+  msg.channel.send(`${numOne} - ${numTwo} = ${diff}`);
 };
