ee0698145d4ba842347f7180a365fc0c3b23b18d
The Grand Dog <alex.h@me.com>
2017-08-29 10:00:06 -0400

    removal of guildConfig option on command configs

--- commands/config/subcom/read/index.js
+++ commands/config/subcom/read/index.js
@@ -1,3 +1,14 @@
 exports.func = (client, msg, args) => {
-
+  client.guildsDB.get(msg.guild.id).then(guildConfig => {
+    guildConfig = JSON.parse(guildConfig);
+    if (args) {
+      val = guildConfig[args[0]]
+      if (val) {
+        val = JSON.stringify(guildConfig[args[0]])
+        msg.channel.send(`this guilds ${args[0]} config is: ${val}`)
+      } else {
+        msg.channel.send(`no such config ${args[0]}`);
+      }
+    }
+  });
 }
