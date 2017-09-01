299fbb78a09aa07ea5a273274bc9c5218d9fd682
The Grand Dog <alex.h@me.com>
2017-09-01 14:16:46 -0400

    functional help command

--- commands/help/index.js
+++ commands/help/index.js
@@ -1,31 +1,59 @@
 exports.func = (client, msg, args) => {
-  if (args) {
-    let helps = getOneHelp(client,args);
-    return msg.channel.send(`${helps.usage}:\n${helps.description}.`)
-  } else {
-    let helps = {};
-    helps = getAllHelp(client.commands, helps);
-  }
+  client.guildsDB.get(msg.guild.id).then(guild => {
+    let commands = client.helpers.clone(client.commands)
 
-  function getAllHelp(coms, helps) {
-    console.log('coms: ', coms);
-    for (var comName in coms) {
-      com = coms[comName];
-      // console.log('comName: ', comName,'\ncom: ', com);
-      helps[comName] = {};
-      helps[comName] = findHelp(com, comName)
+    if (args) {
+      arg = args.shift()
+      if (!commands[arg]) return
+      let helps = getOneHelp(commands[arg],args)
+      console.log('helps: ',helps)
+      return msg.channel.send(`${helps.usage.replace('%prefix%',guild.prefix)}:\n${helps.description}.`)
+    } else {
+      let mainHelps = getAllHelp(commands)
+      let helpMessage = formMainHelp(0,mainHelps)
+      return msg.channel.send(helpMessage)
     }
-    // console.log('helps: ', helps);
-    return helps;
-  }
 
-  function findHelp(com, comName) {
-    let helps = com.help;
-    if (com.subCom) {
-      helps.subCom = {};
-      helps.subCom = getAllHelp(com.subCom, helps)
+    function getOneHelp(com, args) {
+      if (!args[0]) return com.help
+      arg = args.shift()
+      if (!com.subCom[arg]) return com.help
+      return getOneHelp(com.subCom[arg],args)
     }
-    console.log('com.help: ', com.help);
-    return helps;
-  }
+
+    function getAllHelp(coms) {
+      let allHelps = {}
+      for (var name in coms) {
+        com = coms[name]
+        allHelps[name] = findHelp(com)
+      }
+      return allHelps
+    }
+
+    function findHelp(com) {
+      let helps = com.help
+      if (com.subCom) {
+        helps.subCom = {}
+        helps.subCom = getAllHelp(com.subCom)
+      }
+      return helps
+    }
+
+    function formMainHelp(depth,mainHelpObj) {
+      let indent = ''
+      for (var i = 0; i < depth; i++) {
+        indent += '\t'
+      }
+      let levelsHelps = ''
+      for (var key in mainHelpObj) {
+        let currentHelp = mainHelpObj[key]
+        let subComExists = typeof currentHelp.subCom !== 'undefined'
+        levelsHelps += `${indent}**${currentHelp.usage.replace('%prefix%',guild.prefix)}**: ${currentHelp.shortDescription}\n`
+        if (subComExists) {
+          levelsHelps += `${formMainHelp(depth+1,currentHelp.subCom)}`
+        }
+      }
+      return levelsHelps
+    }
+  })
 }
